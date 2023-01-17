00f789279a4cc4884043f84cbfa3b664c3d4ada5
Deny <58119882+Danutopia@users.noreply.github.com>
2023-01-17 07:54:58 +0100

    my first commit

---
+++ TierGehege/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TierGehege
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        UserControl1 userControl1 = new UserControl1();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Zoo.Content = userControl1;
+        }
+
+    }
+}
---
+++ TierGehege/Tier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TierGehege
+{
+    public class Tier
+    {
+        int leben = 100;
+        int hunger = 100;
+        int liebe = 100;
+    }
+}
